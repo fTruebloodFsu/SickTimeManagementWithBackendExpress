96403087092c4681f0eb978f7670968e917d07d4
fTruebloodFsu <39035131+fTruebloodFsu@users.noreply.github.com>
2020-04-27 18:19:06 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/src/index.js'));
 });
 module.exports = router;
