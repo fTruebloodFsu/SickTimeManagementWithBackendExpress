d0309fc58c4df1940997c50b5ccec5daea24b755
fTruebloodFsu <39035131+fTruebloodFsu@users.noreply.github.com>
2020-04-29 22:46:24 -0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 
 const pool = new Pool({
-	process.env.DATABASE_URL,
+	connectionString: process.env.DATABASE_URL,
 //     user: 'gpxuxminieypwk',
 //     host: 'ec2-18-233-137-77.compute-1.amazonaws.com',
 //     database: 'djtftmcbe2ish',
