16eac8298472abe305f19fcd736f269bf1540245
fTruebloodFsu <39035131+fTruebloodFsu@users.noreply.github.com>
2020-05-03 20:39:24 -0700

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var getUserByNameRouter = require('./routes/getUserByName');
 var enterNewRequestRouter = require('./routes/enterNewRequest');
+var deleteEntryRouter = require('./routes/deleteEntry');
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/getUserByName', getUserByNameRouter);
 app.use('/enterNewRequest', enterNewRequestRouter);
+app.use('/deleteEntry', deleteEntryRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
