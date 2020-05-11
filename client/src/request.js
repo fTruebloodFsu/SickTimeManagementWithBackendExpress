c3c2a30abe3422ca09179cd76c59d7f281254666
fTruebloodFsu <39035131+fTruebloodFsu@users.noreply.github.com>
2020-05-11 13:57:57 -0700

    Update request.js

--- client/src/request.js
+++ client/src/request.js
@@ -3,6 +3,8 @@ import { Container, Row, Col, Button } from 'reactstrap';
 import './index.css';
 import "react-datepicker/dist/react-datepicker.css";
 
+const mySecret = process.env.SECRET_CODE;
+
 const formStyle = {
     backgroundColor: '#212529', 
     color: 'white',
@@ -118,7 +120,7 @@ class MyForm extends React.Component {
             this.state.StoreNumber, this.state.selectedDate,
             this.state.Shift, this.state.HoursUsed, this.state.Code);
         
-        if(allFalse(errorsSub) && this.state.Code == process.env.secret_code){
+        if(allFalse(errorsSub) && (this.state.Code == mySecret)){
             //alert("no errors");
             let fn = removeInvalidChars(this.state.FirstName);
             let ln = removeInvalidChars(this.state.LastName);
