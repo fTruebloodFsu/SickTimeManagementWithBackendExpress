6c7e954fb2750d0f012de0b19a9bc25e4f5cfbf0
fTruebloodFsu <39035131+fTruebloodFsu@users.noreply.github.com>
2020-06-16 15:02:38 -0700

    Add files via upload

---
+++ routes/getRequestByStore.js
@@ -0,0 +1,30 @@
+var Pool = require('pg').Pool
+var express = require('express');
+var router = express.Router();
+
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'postgres',
+    password: 'Colby5892::PG',
+    port: 5432,
+})
+
+/* GET users listing. */
+router.post('/', function(req, res, next) {
+    
+    const { begindate, enddate, storenum } = req.body;
+    console.log(req.body.begindate);
+  
+    pool.query('SELECT * FROM sick_time WHERE requestdate >= $1 AND requestdate <= $2 AND storenum = $3', 
+		  [begindate, enddate, storenum], (error, results) => {
+		  if (error) {
+              throw error;
+              console.log(error);
+		  }
+		  res.status(200).json(results.rows)
+		  })
+    });
+
+module.exports = router;
